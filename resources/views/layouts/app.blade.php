4fe02decb4c89b8af2477a8335c7a85eaf85388a
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2025-02-13 19:21:29 -0300

    Arquivos thema 2 e implementacao no layout app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,30 +7,64 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="{{asset('thema/assets/img/favicon/favicon.ico')}}" />
+
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+        href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+        rel="stylesheet"
+        />
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <!-- Icons. Uncomment required icon fonts -->
+        <link rel="stylesheet" href="{{asset('thema/assets/vendor/fonts/boxicons.css')}}" />
+
+        <!-- Core CSS -->
+        <link rel="stylesheet" href="{{asset('thema/assets/vendor/css/core.css')}}" class="template-customizer-core-css" />
+        <link rel="stylesheet" href="{{asset('thema/assets/vendor/css/theme-default.css')}}" class="template-customizer-theme-css" />
+        <link rel="stylesheet" href="{{asset('thema/assets/css/demo.css')}}" />
+
+        <!-- Vendors CSS -->
+        <link rel="stylesheet" href="{{asset('thema/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css')}}" />
+
+        <link rel="stylesheet" href="{{asset('thema/assets/vendor/libs/apex-charts/apex-charts.css')}}" />
+
+        <!-- Page CSS -->
+
+        <!-- Helpers -->
+        <script src="{{asset('thema/assets/vendor/js/helpers.js')}}"></script>
+
+        <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+        <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+        <script src="{{asset('thema/assets/js/config.js')}}"></script>
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+    <body>
+        
+        {{ $slot }}
 
-            <!-- Page Heading -->
-            @isset($header)
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endisset
+        <!-- Core JS -->
+        <!-- build:js assets/vendor/js/core.js -->
+        <script src="{{asset('thema/assets/vendor/libs/jquery/jquery.js')}}"></script>
+        <script src="{{asset('thema/assets/vendor/libs/popper/popper.js')}}"></script>
+        <script src="{{asset('thema/assets/vendor/js/bootstrap.js')}}"></script>
+        <script src="{{asset('thema/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.js')}}"></script>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+        <script src="{{asset('thema/assets/vendor/js/menu.js')}}"></script>
+        <!-- endbuild -->
+
+        <!-- Vendors JS -->
+        <script src="{{asset('thema/assets/vendor/libs/apex-charts/apexcharts.js')}}"></script>
+
+        <!-- Main JS -->
+        <script src="{{asset('thema/assets/js/main.js')}}"></script>
+
+        <!-- Page JS -->
+        <script src="{{asset('thema/assets/js/dashboards-analytics.js')}}"></script>
+
+        <!-- Place this tag in your head or just before your close body tag. -->
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
+    
     </body>
 </html>
