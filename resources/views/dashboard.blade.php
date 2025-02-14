537d27319c7c05c36cf88c79843f21cf9d67045c
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2025-02-13 21:24:45 -0300

    Botoes de ação na tela principal

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,28 +5,57 @@
             <div class="card">
               <div class="d-flex align-items-end row">
                 <div class="col-sm-7">
-                  <div class="card-body">
-                    <h5 class="card-title text-primary">Congratulations John! 🎉</h5>
-                    <p class="mb-4">
-                      You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
-                      your profile.
-                    </p>
+                    <div class="card-body">
+                      <h5 class="card-title text-primary">
+                        Seja bem-vindo {{ Auth::user()?->name ?? 'Desconhecido' }}! 🎉
+                      </h5>
+                    </div>
+                  </div>
+                  
+                    <div class="col-sm-5 text-center text-sm-left">
+                    <div class="card-body pb-0 px-0 px-md-4">
+                        <img
+                        src="{{asset('thema/assets/img/illustrations/man-with-laptop-light.png')}}"
+                        height="140"
+                        alt="View Badge User"
+                        data-app-dark-img="illustrations/man-with-laptop-dark.png"
+                        data-app-light-img="illustrations/man-with-laptop-light.png"
+                        />
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-                    <a href="javascript:;" class="btn btn-sm btn-outline-primary">View Badges</a>
-                  </div>
-                </div>
-                <div class="col-sm-5 text-center text-sm-left">
-                  <div class="card-body pb-0 px-0 px-md-4">
-                    <img
-                      src="{{asset('thema/assets/img/illustrations/man-with-laptop-light.png')}}"
-                      height="140"
-                      alt="View Badge User"
-                      data-app-dark-img="illustrations/man-with-laptop-dark.png"
-                      data-app-light-img="illustrations/man-with-laptop-light.png"
-                    />
-                  </div>
-                </div>
-              </div>
+        <!-- Icon container -->
+        <div class="d-flex flex-wrap" id="icons-container">
+            <!-- Users -->
+            <div class="card icon-card cursor-pointer text-center mb-4 mx-2">
+              <a class="card-body" href="javascript:void(0)">
+                <i class="bx bx-collection mb-2"></i>
+                <p class="icon-name text-capitalize text-truncate mb-0">Usuários</p>
+              </a>
+            </div>
+            <!-- Clients -->
+            <div class="card icon-card cursor-pointer text-center mb-4 mx-2">
+                <a class="card-body" href="javascript:void(0)">
+                    <i class="bx bx-collection mb-2"></i>
+                    <p class="icon-name text-capitalize text-truncate mb-0">Clientes</p>
+                </a>
+            </div>
+            <!-- Colaborators -->
+            <div class="card icon-card cursor-pointer text-center mb-4 mx-2">
+                <a class="card-body" href="javascript:void(0)">
+                    <i class="bx bx-collection mb-2"></i>
+                    <p class="icon-name text-capitalize text-truncate mb-0">Colaboradores</p>
+                </a>
+            </div>
+            <!-- Works -->
+            <div class="card icon-card cursor-pointer text-center mb-4 mx-2">
+                <a class="card-body" href="javascript:void(0)">
+                    <i class="bx bx-collection mb-2"></i>
+                    <p class="icon-name text-capitalize text-truncate mb-0">Serviços</p>
+                </a>
             </div>
         </div>
     </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -177,7 +177,7 @@
                 </div>
     
                 <div class="navbar-nav-right d-flex align-items-center" id="navbar-collapse">
-                  <!-- Search -->
+                  {{-- <!-- Search -->
                   <div class="navbar-nav align-items-center">
                     <div class="nav-item d-flex align-items-center">
                       <i class="bx bx-search fs-4 lh-0"></i>
@@ -189,7 +189,7 @@
                       />
                     </div>
                   </div>
-                  <!-- /Search -->
+                  <!-- /Search --> --}}
     
                   <ul class="navbar-nav flex-row align-items-center ms-auto">
                     <!-- User -->
@@ -209,8 +209,8 @@
                                 </div>
                               </div>
                               <div class="flex-grow-1">
-                                <span class="fw-semibold d-block">John Doe</span>
-                                <small class="text-muted">Admin</small>
+                                <span class="fw-semibold d-block">{{ Auth::user()?->name ?? 'Desconhecido' }}</span>
+                                <small class="text-muted">{{ Auth::user()?->role ?? 'Sem permissão' }}</small>
                               </div>
                             </div>
                           </a>
@@ -219,33 +219,9 @@
                           <div class="dropdown-divider"></div>
                         </li>
                         <li>
-                          <a class="dropdown-item" href="#">
-                            <i class="bx bx-user me-2"></i>
-                            <span class="align-middle">My Profile</span>
-                          </a>
-                        </li>
-                        <li>
-                          <a class="dropdown-item" href="#">
-                            <i class="bx bx-cog me-2"></i>
-                            <span class="align-middle">Settings</span>
-                          </a>
-                        </li>
-                        <li>
-                          <a class="dropdown-item" href="#">
-                            <span class="d-flex align-items-center align-middle">
-                              <i class="flex-shrink-0 bx bx-credit-card me-2"></i>
-                              <span class="flex-grow-1 align-middle">Billing</span>
-                              <span class="flex-shrink-0 badge badge-center rounded-pill bg-danger w-px-20 h-px-20">4</span>
-                            </span>
-                          </a>
-                        </li>
-                        <li>
-                          <div class="dropdown-divider"></div>
-                        </li>
-                        <li>
                           <a class="dropdown-item" href="auth-login-basic.html">
                             <i class="bx bx-power-off me-2"></i>
-                            <span class="align-middle">Log Out</span>
+                            <span class="align-middle">Desconectar</span>
                           </a>
                         </li>
                       </ul>
