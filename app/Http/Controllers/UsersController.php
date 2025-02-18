cd5ee7ba876d58c36c585744682756e819396f5a
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2025-02-18 15:51:05 -0300

    unactivate auth routes

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -7,6 +7,9 @@ use App\Http\Controllers\Controller;
 use Exception;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Validation\Rules;
+use Illuminate\Support\Facades\Hash;
 
 class UsersController extends Controller
 {
@@ -23,17 +26,9 @@ class UsersController extends Controller
     {
         try {
             $validator = Validator::make($request->all(), [
-                'name' => 'required|string|max:255',
-                'email' => 'required|email|unique:users,email',
-                'password' => 'required|min:6|confirmed',
-            ], [
-                'name.required' => 'O campo nome é obrigatório.',
-                'email.email' => 'O campo e-mail deve ser um endereço de e-mail válido.',
-                'email.required' => 'O campo e-mail é obrigatório.',
-                'email.unique' => 'Este e-mail já está em uso.',
-                'password.required' => 'A senha é obrigatória.',
-                'password.min' => 'A senha deve ter pelo menos 6 caracteres.',
-                'password.confirmed' => 'As senhas não coincidem.',
+                'name' => ['required', 'string', 'max:255'],
+                'email' => ['required', 'string', 'lowercase', 'email', 'max:255', 'unique:'.User::class],
+                'password' => ['required', 'confirmed', Rules\Password::defaults()],
             ]);            
         
             if ($validator->fails()) {
@@ -45,8 +40,10 @@ class UsersController extends Controller
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
-                'password' => bcrypt($request->password),
+                'password' => Hash::make($request->password),
             ]);
+
+            event(new Registered($user));
         
             return response()->json([
                 'message' => 'Usuário cadastrado com sucesso!',
