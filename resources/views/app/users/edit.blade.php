0be46913c12eb2c6bb45fccb63bd7ca379d4512b
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2025-02-18 13:17:47 -0300

    Ajuste formulario

--- resources/views/app/users/edit.blade.php
+++ resources/views/app/users/edit.blade.php
@@ -6,24 +6,27 @@
             {{-- <small class="text-muted float-end">Default label</small> --}}
         </div>
         <div class="card-body">
-            <form>
-            <div class="mb-3">
-                <label class="form-label" for="basic-default-fullname">Nome</label>
-                <input type="text" class="form-control" id="basic-default-fullname" name="name" placeholder="João Doe" />
-            </div>
-            <div class="mb-3">
-                <label class="form-label" for="basic-default-company">Email</label>
-                <input type="email" class="form-control" id="basic-default-company"name="email" placeholder="exemplo@exemplo.com" />
-            </div>
-            <div class="mb-3">
-                <label class="form-label" for="basic-default-company">Senha</label>
-                <input type="password" class="form-control" id="basic-default-company"name="password" />
-            </div>
-            <div class="mb-3">
-                <label class="form-label" for="basic-default-company">Confirmar Senha</label>
-                <input type="password" class="form-control" id="basic-default-company"name="confirm_password" />
-            </div>
-            <button type="submit" class="btn btn-primary">Salvar</button>
+            <form id="form-cadastro-usuario">
+                <div class="mb-3">
+                    <label class="form-label" for="basic-default-fullname">Nome</label>
+                    <input type="text" class="form-control" id="basic-default-fullname" name="name" placeholder="João Doe" />
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="basic-default-company">Email</label>
+                    <input type="email" class="form-control" id="basic-default-company"name="email" placeholder="exemplo@exemplo.com" />
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="basic-default-company">Senha</label>
+                    <input type="password" class="form-control" id="basic-default-company"name="password" />
+                </div>
+                <div class="mb-3">
+                    <label class="form-label" for="basic-default-company">Confirmar Senha</label>
+                    <input type="password" class="form-control" id="basic-default-company"name="confirm_password" />
+                    <div class="form-text">A confirmação de senha deve ser igual a senha!</div>
+                </div>
+                <div class="d-flex justify-content-end">
+                    <button type="submit" class="btn btn-primary right">Salvar</button>
+                </div>
             </form>
         </div>
         </div>
