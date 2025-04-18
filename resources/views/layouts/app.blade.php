eee94d93eee8c3c2acc5d00c743dc374abd9f8dd
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2025-04-18 10:16:24 -0300

    livewire table

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,12 +40,18 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
 
+    @livewireStyles
+
     <!-- Adds the Core Table Styles -->
     @rappasoftTableStyles
 
     <!-- Adds any relevant Third-Party Styles (Used for DateRangeFilter (Flatpickr) and NumberRangeFilter) -->
     @rappasoftTableThirdPartyStyles
-    
+
+    <style>
+      [x-cloak] { display: none !important; }
+    </style>
+
     <script src="{{asset('thema/assets/js/config.js')}}"></script> 
   </head>
 
