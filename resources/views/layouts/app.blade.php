e4d87c29639144495174df94a16217e90fa1ac70
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2025-04-18 10:37:32 -0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,15 +40,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
 
-    @livewireStyles
-
-    <!-- Adds any relevant Third-Party Styles (Used for DateRangeFilter (Flatpickr) and NumberRangeFilter) -->
-    @rappasoftTableThirdPartyStyles
-
-    <style>
-      [x-cloak] { display: none !important; }
-    </style>
-
     <script src="{{asset('thema/assets/js/config.js')}}"></script> 
   </head>
 
@@ -285,8 +276,6 @@
     <!-- Input Mask -->
     <script src="https://cdn.jsdelivr.net/npm/inputmask@5.0.6/dist/inputmask.min.js"></script>
 
-    <!-- Adds any relevant Third-Party Scripts (e.g. Flatpickr) -->
-    @rappasoftTableThirdPartyScripts
   </body>
 
 </html>
