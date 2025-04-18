082ff14e31e0dc6c0b37270707ef29995198a332
Vinícius De Senna <110927988+ViniciusDeSenna@users.noreply.github.com>
2025-04-18 10:54:19 -0300

    fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -40,6 +40,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
 
+    @livewireStyles
+
     <script src="{{asset('thema/assets/js/config.js')}}"></script> 
   </head>
 
@@ -232,8 +234,8 @@
     </div>
     <!-- / Layout wrapper -->
 
-    @livewireStyles
     @livewireScripts
+
 
     <!-- Core JS -->
     <!-- build:js assets/vendor/js/core.js -->
